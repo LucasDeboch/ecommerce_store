d66ab825ab88d65a00e5aff3b2464c788d26de2b
Lucas Dola Deboch <39872666+LucasDeboch@users.noreply.github.com>
2025-10-25 10:53:14 +0300

    Delete the meta class in store model

--- store/models.py
+++ store/models.py
@@ -35,11 +35,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_STATUS, default=MEMBERSHIP_BRONZE)
     
-    class Meta:
-        db_table = "store_customers"
-        indexes = [
-            models.Index(fields=['first_name', "last_name"])
-        ]
+    
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = "P"
