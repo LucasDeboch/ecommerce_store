7740158c3c56df947e368a0cbf43fbe1269e9eb6
Lucas Dola Deboch <39872666+LucasDeboch@users.noreply.github.com>
2025-10-26 02:50:31 +0300

    ORM finished

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.http import HttpResponse
 from store.models import Product
+from store.models import Customer
 
 # Create your views here.
 def hello_world(request):
     
-    product = Product.objects.get(pk=1)
-    return render(request, 'hello.html', {"name": "Lucas", "age": 27})
+    customers = Customer.objects.all().order_by("-first_name")[:10]
+    result = Product.objects.all().aggregate(count=Count(id), price=Max('price'))
+    queryset = Product.objects.all().order_by('title')
+    return render(request, 'hello.html', {"name": "Lucas", "age": 27, "products": list(queryset), "customers": list(customers), "result": result})
 
